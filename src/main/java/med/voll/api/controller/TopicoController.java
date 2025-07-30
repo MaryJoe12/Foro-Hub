0f48a6d7cc175cc2c74ecc52fae4f6e82db58947
Marijo <mjmc12.22@gmail.com>
2025-07-29 22:16:14 -0600

    variables

--- src/main/java/med/voll/api/controller/TopicoController.java
+++ src/main/java/med/voll/api/controller/TopicoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-
+//prueba
 @RestController
 @RequestMapping("/topicos")
 @SecurityRequirement(name = "bearer-key")
